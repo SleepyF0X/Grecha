197ef5a9df2092b75bd4549a617a9affc84560f2
Victor <victorrar.ru@gmail.com>
2021-01-24 16:35:00 +0200

    Add parser interface

---
+++ Grecha/Parsing/IParser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using DAL.Models;
+
+namespace Grecha.Parsing
+{
+    public interface IParser
+    {
+        public List<Product> Parse(string keyword, int? limit, int? offset);
+    }
+}
