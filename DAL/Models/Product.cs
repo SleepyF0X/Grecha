051ce25bdf4b9f153770962c102ee5efb4ad7f6c
Егор Павшук <egor.pavshuk@outlook.com>
2021-01-25 20:15:38 +0200

    Update Fozzy parser

--- DAL/Models/Product.cs
+++ DAL/Models/Product.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public string Shop { get; set; }
-        public int IdFromShop { get; set; }
         public string Name { get; set; }
         public string TradeMark { get; set; }
         public double Price { get; set; } //TODO switch to the fixed point
+        public string Img { get; set; }
     }
 }
---
+++ Grecha/Parsing/FozzyParser.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using DAL.Models;
+using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+
+namespace Grecha.Parsing
+{
+    public class FozzyParser : IParser
+    {
+        public List<Product> Parse(string keyword, int? limit = Int32.MaxValue, int? offset = 0)
+        {
+            var htmlWeb = new HtmlWeb();
+            var html = htmlWeb.Load("https://fozzyshop.ua/ru/search?controller=search&s=" + keyword);
+            var document = html.DocumentNode;
+            var allPages = document.QuerySelectorAll(".page-list > li");
+            List<int> numbers = new List<int>();
+            int numberOfPages = 1;
+
+            
+            foreach (var page in allPages)
+            {
+                
+                if (page.QuerySelector("a > i") == null && page.QuerySelector("span") == null)
+                {
+                    numbers.Add(Convert.ToInt32(page.QuerySelector("a").InnerText));
+                }
+                
+            }
+
+            if(numbers.Count != 0)
+                numberOfPages = numbers.Max();
+
+            List<Product> products = new List<Product>();
+            bool isAble = true;
+
+            for (int i = 1; i <= numberOfPages; i++) 
+            {
+                html = htmlWeb.Load("https://fozzyshop.ua/ru/search?controller=search&page=" + i + "&s=" + keyword);///
+                document = html.DocumentNode;
+                var list = document.QuerySelectorAll(".js-product-miniature-wrapper");
+
+                
+                foreach (var elem in list)
+                {
+                    if (!elem.HasClass("product_grey"))  
+                    {
+                        Product product = new Product();
+                        product.Shop = "Fozzy";
+                        product.Name = GetProductName(elem.QuerySelector(".product-title > a")
+                            .GetAttributeValue("href", string.Empty));
+                        product.TradeMark = elem.QuerySelector(".product-brand > a").InnerText;
+                        var htmlPrice = elem.QuerySelector(".product-price").Attributes["content"].Value.Replace(".",",");
+                        product.Price = Convert.ToDouble(htmlPrice);
+                        product.Img = elem.QuerySelector(".thumbnail-container > a > img").Attributes["src"].Value;
+
+                        products.Add(product);
+                    }
+                    else
+                    {
+                        isAble = false;
+                        break;
+                    }
+                }
+                if(!isAble)
+                    break;
+            }
+
+            return products;
+        }
+
+        private string GetProductName(string url)
+        {
+            var htmlWeb = new HtmlWeb();
+            var html = htmlWeb.Load(url);
+            var document = html.DocumentNode;
+            var name = document.QuerySelector(".page-title > span").InnerText.Replace("&quot;","'");
+            return name;
+        }
+    }
+}
